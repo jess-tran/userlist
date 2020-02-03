ffdadbf55b6b04daf532819f7e30e084c7d3ef36
Jessica Tran <jessicatran2011@gmail.com>
2020-02-03 10:25:07 -0800

    committed backend

---
+++ README.md
@@ -0,0 +1,10 @@
+Backend
+Created a middlelayer server in ExpressJS that retrieves a list of users from 
+https://jsonplaceholder.typicode.com/users.
+Include unit tests to validate the implementation.
+
+
+Frontend
+Simple web application on ReactJS that etrieves a list of users from the Express backend.
+Each button has the user's first and last name.
+When clicked, it toggles the user's phone number and address below the user's first and last name. 
---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const users = require("./data");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+const filteredUsers = users.map(({ id, name, address, phone }) => {
+  return {
+    id,
+    name,
+    address,
+    phone
+  };
+});
+
+app.get("/api/users", (req, res) => {
+  if (filteredUsers) {
+    res.json(filteredUsers);
+  } else {
+    res.status(400).json(`Error: ${error}`);
+  }
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find(user => user.id === parseInt(req.params.id));
+
+  if (user) {
+    res.json(filteredUsers.filter(user => user.id === parseInt(req.params.id)));
+  } else {
+    res.status(404).json({
+      message: `The user with the id of ${req.params.id} was not found.`
+    });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
